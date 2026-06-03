1e260bd81cd1e81296545eceb472a12b2dadeaf1
christinexiaxiaxia <christine-xia@hotmail.com>
2026-06-02 23:32:40 -0400

    commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// CURSOR TEXT
+
+$(document).mousemove(function(e){
+	$('.bye-concrete').css({'top': e.clientY + 10, 'left': e.clientX + 10})
+})
+
+$(document).mousemove(function(e){
+	$('.download').css({'top': e.clientY + 10, 'left': e.clientX + 10})
+})
+
+$('.letter').hover(
+	function(){
+		$('.bye-concrete').css({'display':'none'})
+		$('.download').css({'display':'block'})
+	}, function(){
+		$('.bye-concrete').css({'display':'block'})
+		$('.download').css({'display':'none'})
+	}
+)
+
+
+
+
+// HOVER IMAGE following mouse
+
+$(document).mousemove(function(e){
+	$('.hoverimg').css({'top': e.clientY + 16, 'left': e.clientX - 0})
+})
+
+///////////
+
+$('.header.more.intro.defunkt1').hover(
+	function(){
+		$('.hoverimg.defunkt1').css({'display':'block'})
+		$('.hi-concrete').css({'display':'none'})
+	},
+	function(){
+		$('.hoverimg.defunkt1').css({'display':'none'})
+		$('.hi-concrete').css({'display':'block'})
+	}
+)
